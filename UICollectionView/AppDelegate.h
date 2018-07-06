21928f62e0fcba1982e8c35feea0f219edc8c685
Cassandra Le <lenguyetminh1998@gmail.com>
2018-07-06 15:04:31 -0700

    Initial Commit

---
+++ UICollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView
+//
+//  Created by Cassandra Le on 7/6/18.
+//  Copyright © 2018 Cassandra Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionView
+//
+//  Created by Cassandra Le on 7/6/18.
+//  Copyright © 2018 Cassandra Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
