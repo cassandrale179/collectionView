68daba09aafcfa72cfd7985cc08770a4ec8f12d0
Cassandra Le <lenguyetminh1998@gmail.com>
2018-07-07 12:41:36 -0700

    I miss Ionic

--- UICollectionView/ViewController.m
+++ UICollectionView/ViewController.m
@@ -1,29 +1,56 @@
-//
 //  ViewController.m
 //  UICollectionView
-//
-//  Created by Cassandra Le on 7/6/18.
-//  Copyright © 2018 Cassandra Le. All rights reserved.
-//
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UICollectionViewDelegate, UICollectionViewDataSource>
+
+
+@property NSArray *image_Arr;
+@property NSArray *label_Arr;
 
 @end
 
+
+
 @implementation ViewController
 
+
+
+
+// Initialize Array and Labels
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _image_Arr = [[NSArray alloc] initWithObjects:@"image1", @"image2", @"image3", @"image4", nil];
+    _label_Arr = [[NSArray alloc] initWithObjects: @"One", @"Two", @"Three", @"Four", nil];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+
+// Return number of items in array
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return _image_Arr.count;
+}
+
+
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell_ID" forIndexPath:indexPath];
+    UIImageView *Image_View = (UIImageView *)[cell viewWithTag:100];
+    UILabel *Label = (UILabel *)[cell viewWithTag:101];
+    Image_View.image = [UIImage imageNamed:[_image_Arr objectAtIndex:indexPath.row]];
+    Label.text = [_label_Arr objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+
 
 
 @end
