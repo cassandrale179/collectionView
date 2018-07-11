62eff3876a661222542e2a6b5cdc415b966fdf50
cassandrale179 <lenguyetminh1998@gmail.com>
2018-07-11 15:47:22 -0700

    DONE: add header

--- UICollectionView/ViewController.m
+++ UICollectionView/ViewController.m
@@ -12,21 +12,40 @@ const int totalImages = 9.0;
 
 #pragma mark - collectionView methods
 // This will be called 9 times, each time for 1 cell
-- (UICollectionViewCell *) collectionView:(UICollectionView *) collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+- (UICollectionViewCell *) collectionView:(UICollectionView *) collectionView
+                   cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     ImageCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
     cell.imageView.image = [UIImage imageNamed:[_image_Arr objectAtIndex:indexPath.row]];
-//    cell.layer.borderWidth=1.0f;
-//    cell.layer.borderColor=[UIColor blueColor].CGColor;
     return cell;
 }
 
 // Return number of items in array
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    NSLog(@"Return total images");
     return totalImages;
 }
 
+// Set a size for the header file first
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+referenceSizeForHeaderInSection:(NSInteger)section {
+    NSLog(@"layout called");
+    return CGSizeMake(60.0f, 60.0f);
+}
+
+
+// Link the header of collection view to the header we created (through the identifier) 
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView
+           viewForSupplementaryElementOfKind:(NSString *)kind
+                                 atIndexPath:(NSIndexPath *)indexPath{
+        UICollectionReusableView* view = [collectionView dequeueReusableSupplementaryViewOfKind: kind withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
+        return view;
+}
+
+
 #pragma mark - view methods
 - (void)viewDidLoad{
+    NSLog(@"View did load");
     [super viewDidLoad];
 
     // Dynamically create name
@@ -41,9 +60,5 @@ const int totalImages = 9.0;
     self.Collection_view.collectionViewLayout = [[CustomImageFlowLayout alloc] init];
 
 }
-
-
-
-
 
 @end
